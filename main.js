ab3d459a79446889000ceee8dbfb3e3f4f82f1c8
tatyzin29 <110253746+tatyzin29@users.noreply.github.com>
2024-03-15 11:44:32 -0300

    atualização 15/03

--- main.js
+++ main.js
@@ -38,11 +38,11 @@ function calculaTempo(tempoObjetivo) {
         return [dias,horas,minutos,segundos];
     } else {
         return [0,0,0,0];
-    }
+    }           
 }
 
 function atualizaCronometro(){
-    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
+    document.getElementById("dias").textContent = calculaTempo(tempos[0])[0];
     document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
     document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
     document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
